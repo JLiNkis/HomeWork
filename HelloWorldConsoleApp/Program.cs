ad0959fc63c8c44a2c3ba36119f9b787bb8867d5
JLiNkis <160965508+JLiNkis@users.noreply.github.com>
2024-05-07 01:29:04 +0300

    Add files via upload

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿// Импорт сторонних пространств имён
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Пространство имён
+/// Создание собственной библиотеки
+/// </summary>
+namespace HelloWorldConsoleApp
+{
+    /// <summary>
+    /// Главная составляющая единица приложения - класс
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Код, который выполняется в первую очередь
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow "); // Вывод текста в консоль
+
+            Console.WriteLine("World ");
+                    
+            Console.WriteLine("!!!");
+
+            Console.Write("Hellow ");
+               
+            Console.Write("World ");
+                    
+            Console.Write("!!!");
+
+            Console.ReadKey(); // Ожидание от пользователя нажатия какой-либокнопки
+        }
+    }
+}
